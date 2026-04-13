4f55cc71a1052ebd6d178f2388b46332fa63151b
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2026-04-13 17:11:53 -0300

    Traffic light interface

--- setup.py
+++ setup.py
@@ -4,20 +4,30 @@ from glob import glob
 
 package_name = 'vilma_hmi'
 
-setup(
-    name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
-    data_files=[
+def package_files(directory):
+    paths = []
+    for (path, _, filenames) in os.walk(directory):
+        install_path = os.path.join('share', package_name, path)
+        files = [os.path.join(path, f) for f in filenames]
+        if files:
+            paths.append((install_path, files))
+    return paths
+
+data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'website'), glob(os.path.join('website', '*.html'))),
-        (os.path.join('share', package_name, 'website'), glob(os.path.join('website', '*.js'))),
-         (os.path.join('share', package_name, 'website'), glob(os.path.join('website', '*.png')) + glob(os.path.join('images', '*.jpg'))),
         (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*')))
-    ],
+    ]
+
+data_files += package_files('website')
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Gabriel Toffanetto',
