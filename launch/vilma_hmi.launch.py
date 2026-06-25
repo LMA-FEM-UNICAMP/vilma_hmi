1fa2ac25a90fffc99dcf75a0143faceb380bd03c
Gabriel Toffanetto <gabrieltoffanetto@gmail.com>
2026-06-25 16:46:26 -0300

    Adding vilma_autoware HMI

--- launch/vilma_hmi.launch.py
+++ launch/vilma_hmi.launch.py
@@ -8,6 +8,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import OpaqueFunction
 
 def load_yaml(yaml_file_path):
     with open(yaml_file_path, 'r') as f:
@@ -23,41 +24,50 @@ def symlink_force(target, link_name):
         else:
             raise e
 
-def generate_launch_description():
-    
-    interface = DeclareLaunchArgument("interface", default_value="vilma_status")
+def launch_setup(context, *args, **kwargs):
     
     server_dir = os.path.join(get_package_share_directory("vilma_hmi"), "website")
     params_dir = os.path.join(get_package_share_directory("vilma_hmi"), "config")
     
-    params = load_yaml(os.path.join(params_dir, LaunchConfiguration("interface")+".param"))
+    interface = LaunchConfiguration("interface").perform(context)
+    
+    params = load_yaml(os.path.join(params_dir, "host.param.yaml"))
     
     rosbridge_ip = params['vilma_hmi']['ros__parameters']['server_ip']
     rosbridge_port = params['vilma_hmi']['ros__parameters']['rosbridge_port']
     hmi_port = params['vilma_hmi']['ros__parameters']['server_port']
-    index = params['vilma_hmi']['ros__parameters']['index']
+    # index = params['vilma_hmi']['ros__parameters']['index']
     
     # Create index.html file
-    symlink_force(os.path.join(server_dir, index), os.path.join(server_dir, 'index.html'))
+    symlink_force(os.path.join(server_dir, interface+".html"), os.path.join(server_dir, 'index.html'))
     
     
     config = {"serviceIp": f"{rosbridge_ip}:{rosbridge_port}"}
     with open(f"{server_dir}/config.json", "w") as f:
         json.dump(config, f)
-
-    return LaunchDescription([
-        interface,
         
-        # Start rosbridge_websocket on port 9090
-        ExecuteProcess(
-            cmd=['ros2', 'launch', 'rosbridge_server', 'rosbridge_websocket_launch.xml', 'port:='+rosbridge_port],
-            output='screen'
-        ),
+    # Start rosbridge_websocket on port 9090
+    launch_rosbridge_server = ExecuteProcess(
+        cmd=['ros2', 'launch', 'rosbridge_server', 'rosbridge_websocket_launch.xml', 'port:='+rosbridge_port],
+        output='screen'
+    )
 
-        # Start Python HTTP server on port hmi_port
-        ExecuteProcess(
-            cmd=['python3', '-m', 'http.server', hmi_port],
-            cwd=server_dir,
-            output='screen'
-        )
-    ])
+    # Start Python HTTP server on port hmi_port
+    launch_http_server = ExecuteProcess(
+        cmd=['python3', '-m', 'http.server', hmi_port],
+        cwd=server_dir,
+        output='screen'
+    )
+        
+    return [launch_rosbridge_server, launch_http_server]
+        
+        
+def generate_launch_description():
+
+    return LaunchDescription(
+        [
+        DeclareLaunchArgument("interface", default_value="vilma_status")
+        ]
+        +
+        [OpaqueFunction(function=launch_setup)]
+    )
